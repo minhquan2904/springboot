aff935fdb0c39af4724e780d05c0bdff5c66a016
minhquan2904 <minhquan2904@outlook.com>
2017-03-25 10:25:38 +0700

    hello world

---
+++ src/main/java/com/hellokoding/springboot/Nhom6Controller.java
@@ -0,0 +1,15 @@
+package com.hellokoding.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Nhom6Controller {
+    @RequestMapping("/h")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
